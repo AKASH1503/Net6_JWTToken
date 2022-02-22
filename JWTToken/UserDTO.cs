3ab62e609202d4621c26ec00bbd4c80122891651
Akash Rasal <rasalakash8@gmail.com>
2022-02-22 15:42:04 +0530

    CompleteInitial

---
+++ JWTToken/UserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace JWTToken
+{
+    public class UserDTO
+    {
+        public string Username { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
+    }
+}
